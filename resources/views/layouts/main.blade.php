f8531197858e75b92f2011a0fe3a25e0db67f926
Ida Bagus Adimas <aridirat@gmail.com>
2026-03-27 09:05:45 +0800

    mengapus file 2026_02_25_170933_add_two_factor_columns_to_users_table yang duplikat dengan migrasi user

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://unpkg.com/heroicons@2.0.18/dist/heroicons.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <title>Halaman Utama</title>
+    <title>Pusat Data Indonesia Bali</title>
 </head>
 <body class="bg-gray-100">
 
