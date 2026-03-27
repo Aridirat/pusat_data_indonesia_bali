93659df3da22a95fa1bb8d68842efededed82374
Ida Bagus Adimas <aridirat@gmail.com>
2026-03-27 12:07:05 +0800

    memperbaiki vite calling di pages auth dan main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if(app()->environment('testing'))
+    {{-- Vite dimatikan saat testing --}}
+    @else
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if(app()->environment('testing'))
+    {{-- Vite dimatikan saat testing --}}
+    @else
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @endif
     <title>Login</title>
 </head>
 
